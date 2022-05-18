c0a37f4dbf0142e073c61f87357f476fb548ba4e
Marcos Vinicius <marcos.oliveira@liberali.com.br>
2022-05-18 16:45:04 -0400

    feat: tela-login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <>
-      <Home />
+      <Login />
+      {/* <Home /> */}
     </>
   );
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react'
+import './styles.css'
+
 export const Login = () => {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const handlelogin = (e) => {
+    
+  }
 
   return (
-    <div>
-      Login Page
+    <div id="container-login">
+      <h1 className="title">Bem vindo, faça seu Login</h1>
+      <form className="form">
+
+        <div className="field">
+          <label htmlFor="email">Email: </label>
+          <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} name="email" id="email"/>
+        </div>
+        <div className="field">
+          <label htmlFor="password">Senha: </label>
+          <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} name="password" id="password"/>
+        </div>
+
+        <div className="container-btn">
+          <button type="submit">LOGIN</button>
+        </div>
+      </form>
     </div>
   )
 }
