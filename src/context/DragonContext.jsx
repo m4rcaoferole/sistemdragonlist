1cb0838f50874c1fe0c5e1a58e42dd72d672e450
Marcos Vinicius <victor_jv380@hotmiail.com>
2022-05-19 21:01:07 -0400

    feat: criado instancia do axios

--- src/context/DragonContext.jsx
+++ src/context/DragonContext.jsx
@@ -1,11 +1,14 @@
+import axios from "axios";
 import { createContext } from "react";
 
-export const DragonContext = createContext()
+export const DragonContext = createContext({})
 
 export const DragonProvidor = ({ children }) => {
-
+  function handleDelete(id) {
+    axios.delete(`api/v1/dragon/${id}`)
+  }
   return (
-    <DragonContext.Provider value={ }>
+    <DragonContext.Provider value={{ handleDelete }}>
       {children}
     </DragonContext.Provider>
   )
